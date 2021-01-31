dae629984857733de9905efda9a996bdf100a6a3
Dong Chen <352070977@qq.com>
2021-01-31 18:56:44 +0800

    添加Exporter

---
+++ Source/LibraryExporter/EmmyLuaLibraryExporter/EmmyLuaLibraryExporter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmmyLuaLibraryExporter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
